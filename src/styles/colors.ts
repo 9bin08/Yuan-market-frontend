f1c562d12987dce9539830b7aeae12b8a5a84b32
김규빈 <9bin08@naver.com>
2023-11-18 18:55:42 +0900

    -added : colors styles 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    white: '#FFFFFF',
+    line: '#ECECEC',
+    black: '#000000',
+
+    text001: '#292929',
+    text002: '#1C1C1C',
+    text003: '#0F1822',
+
+    gray001: '#F5F5F5',
+    gray002: '#EBEDF0',
+    gray003: '#DEE6E7',
+    gray004: '#9F9F9F',
+    gray005: '#757E88',
+
+    key001: '#00B1BB',
+    key002: '#24ABBE',
+
+    red001: '#FF5630',
+}
