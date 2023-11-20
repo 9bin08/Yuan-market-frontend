a78af224fb00aa33caa82e2f2f77894800272c6d
김규빈 <9bin08@naver.com>
2023-11-21 02:36:30 +0900

    -added : @shared icon 컴포넌트 추가

---
+++ src/components/@shared/Icon/index.tsx
@@ -0,0 +1,28 @@
+import * as icons from '@constants/icons';
+
+type IconName = keyof typeof icons;
+
+interface IconProps {
+  name: IconName;
+  size?: number;
+  className?: string;
+  onClick?: () => void;
+}
+
+const Icon = ({ name, size = 24, className, onClick, ...rest }: IconProps) => {
+  const iconSrc = icons[name];
+
+  return (
+    <img
+      alt={name}
+      src={iconSrc}
+      width={size}
+      height={size}
+      onClick={onClick}
+      className={className}
+      {...rest}
+    />
+  );
+};
+
+export default Icon;
